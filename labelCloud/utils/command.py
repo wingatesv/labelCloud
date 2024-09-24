171d1bc8bada2abd35d0b715351cb9809f5e8ac2
Wingates Voon <56868536+wingatesv@users.noreply.github.com>
2024-09-24 16:43:00 +0800

    Create command.py

---
+++ labelCloud/utils/command.py
@@ -0,0 +1,28 @@
+import subprocess
+import os
+import torch
+def run_command(command, target_subdirectory=None):
+    """
+    Execute a command in the shell.
+    """
+    try:
+        # switch to the target subdirectory
+        if target_subdirectory is not None:
+            cwd = os.getcwd()
+            os.chdir(target_subdirectory)
+            print(cwd)
+            print(target_subdirectory)
+            print(os.getcwd())
+            print(command)
+
+        # run the command
+        subprocess.run(command, shell=True, check=True)
+        
+        # switch back to original directory
+        if target_subdirectory is not None:
+            os.chdir(cwd)
+
+        torch.cuda.empty_cache()
+        
+    except subprocess.CalledProcessError as e:
+        print(f"Error executing command: {e}")
